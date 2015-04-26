df96818591c5014c03c6fd9956b13f2fd812d38e
David Janes <davidjanes@davidjanes.com>
2015-04-26 07:35:30 -0400

    npm test cleanup

--- ExpressTransport.js
+++ ExpressTransport.js
@@ -37,6 +37,12 @@ var logger = bunyan.createLogger({
     module: 'ExpressTransport',
 });
 
+/* --- forward definitions --- */
+var _encode;
+var _decode;
+var _unpack;
+var _pack;
+
 /* --- constructor --- */
 
 /**
@@ -53,8 +59,7 @@ var ExpressTransport = function (initd, app) {
     var self = this;
 
     self.initd = _.defaults(
-        initd,
-        {
+        initd, {
             channel: iotdb.transporter.channel,
             unchannel: iotdb.transporter.unchannel,
             encode: _encode,
@@ -62,12 +67,11 @@ var ExpressTransport = function (initd, app) {
             pack: _pack,
             unpack: _unpack,
         },
-        iotdb.keystore().get("/transports/ExpressTransport/initd"),
-        {
+        iotdb.keystore().get("/transports/ExpressTransport/initd"), {
             prefix: "/"
         }
     );
-    
+
     self.native = app;
 
     self._setup_app_thing_band();
@@ -77,18 +81,18 @@ var ExpressTransport = function (initd, app) {
     this._emitter = new events.EventEmitter();
 };
 
-ExpressTransport.prototype = new iotdb.transporter.Transport;
+ExpressTransport.prototype = new iotdb.transporter.Transport();
 
 /* --- web --- */
 
-ExpressTransport.prototype._setup_app_things = function() {
+ExpressTransport.prototype._setup_app_things = function () {
     var self = this;
 
     var channel = self.initd.channel(self.initd);
 
-    self.native.use(channel, function(request, response) {
+    self.native.use(channel, function (request, response) {
         var ids = [];
-        self.list(function(ld) {
+        self.list(function (ld) {
             if (ld.end) {
                 var rd = {
                     "@id": self.initd.channel(self.initd),
@@ -97,8 +101,7 @@ ExpressTransport.prototype._setup_app_things = function() {
 
                 response
                     .set('Content-Type', 'application/json')
-                    .send(JSON.stringify(rd, null, 2))
-                    ;
+                    .send(JSON.stringify(rd, null, 2));
 
                 return;
             }
@@ -108,15 +111,15 @@ ExpressTransport.prototype._setup_app_things = function() {
     });
 };
 
-ExpressTransport.prototype._setup_app_thing = function() {
+ExpressTransport.prototype._setup_app_thing = function () {
     var self = this;
 
     var channel = self.initd.channel(self.initd, ':id');
 
-    self.native.use(channel, function(request, response) {
+    self.native.use(channel, function (request, response) {
         self.about({
-            id: request.params.id, 
-        }, function(ad) {
+            id: request.params.id,
+        }, function (ad) {
             var rd = {
                 "@id": self.initd.channel(self.initd, request.params.id),
             };
@@ -124,7 +127,7 @@ ExpressTransport.prototype._setup_app_thing = function() {
             if ((ad.bands === null) || (ad.bands === undefined)) {
                 response.status(404);
                 rd["error"] = "Not Found";
-            } else if (_.isArray(ad.bands)) {
+            } else if (_.is.Array(ad.bands)) {
                 for (var bi in ad.bands) {
                     var band = ad.bands[bi];
                     rd[band] = self.initd.channel(self.initd, request.params.id, band);
@@ -133,22 +136,21 @@ ExpressTransport.prototype._setup_app_thing = function() {
 
             response
                 .set('Content-Type', 'application/json')
-                .send(JSON.stringify(rd, null, 2))
-                ;
+                .send(JSON.stringify(rd, null, 2));
         });
     });
 };
 
-ExpressTransport.prototype._setup_app_thing_band = function() {
+ExpressTransport.prototype._setup_app_thing_band = function () {
     var self = this;
 
     var channel = self.initd.channel(self.initd, ':id', ':band');
 
-    self.native.get(channel, function(request, response) {
+    self.native.get(channel, function (request, response) {
         self.get({
-            id: request.params.id, 
-            band: request.params.band, 
-        }, function(gd) {
+            id: request.params.id,
+            band: request.params.band,
+        }, function (gd) {
             var rd = {
                 "@id": self.initd.channel(self.initd, request.params.id, request.params.band),
             };
@@ -162,15 +164,14 @@ ExpressTransport.prototype._setup_app_thing_band = function() {
 
             response
                 .set('Content-Type', 'application/json')
-                .send(JSON.stringify(rd, null, 2))
-                ;
+                .send(JSON.stringify(rd, null, 2));
         });
     });
 
-    self.native.put(channel, function(request, response) {
+    self.native.put(channel, function (request, response) {
         self._emitter.emit("updated", {
-            id: request.params.id, 
-            band: request.params.band, 
+            id: request.params.id,
+            band: request.params.band,
             value: request.body,
         });
 
@@ -180,8 +181,7 @@ ExpressTransport.prototype._setup_app_thing_band = function() {
 
         response
             .set('Content-Type', 'application/json')
-            .send(JSON.stringify(rd, null, 2))
-            ;
+            .send(JSON.stringify(rd, null, 2));
     });
 };
 
@@ -194,7 +194,7 @@ ExpressTransport.prototype._setup_app_thing_band = function() {
  *  you should use <code>iotdb.transport.bind</code>
  *  to effectively replace this function.
  */
-ExpressTransport.prototype.list = function(paramd, callback) {
+ExpressTransport.prototype.list = function (paramd, callback) {
     var self = this;
 
     if (arguments.length === 1) {
@@ -215,7 +215,7 @@ ExpressTransport.prototype.list = function(paramd, callback) {
  *  you should use <code>iotdb.transport.bind</code>
  *  to effectively replace this function.
  */
-ExpressTransport.prototype.added = function(paramd, callback) {
+ExpressTransport.prototype.added = function (paramd, callback) {
     var self = this;
 
     if (arguments.length === 1) {
@@ -233,8 +233,7 @@ ExpressTransport.prototype.added = function(paramd, callback) {
  *  you should use <code>iotdb.transport.bind</code>
  *  to effectively replace this function.
  */
-ExpressTransport.prototype.about = function(paramd, callback) {
-};
+ExpressTransport.prototype.about = function (paramd, callback) {};
 
 /**
  *  See {iotdb.transporter.Transport#get} for documentation.
@@ -243,8 +242,7 @@ ExpressTransport.prototype.about = function(paramd, callback) {
  *  you should use <code>iotdb.transport.bind</code>
  *  to effectively replace this function.
  */
-ExpressTransport.prototype.get = function(paramd, callback) {
-};
+ExpressTransport.prototype.get = function (paramd, callback) {};
 
 /**
  *  See {iotdb.transporter.Transport#update} for documentation.
@@ -253,15 +251,14 @@ ExpressTransport.prototype.get = function(paramd, callback) {
  *  you should use <code>iotdb.transport.bind</code>
  *  to effectively replace this function.
  */
-ExpressTransport.prototype.update = function(id, band, value) {
-};
+ExpressTransport.prototype.update = function (id, band, value) {};
 
 /**
  *  See {iotdb.transporter.Transport#updated} for documentation.
  *  <p>
  *  This will be triggered from the Express/Express API
  */
-ExpressTransport.prototype.updated = function(paramd, callback) {
+ExpressTransport.prototype.updated = function (paramd, callback) {
     var self = this;
 
     if (arguments.length === 1) {
@@ -271,7 +268,7 @@ ExpressTransport.prototype.updated = function(paramd, callback) {
 
     self._validate_updated(paramd, callback);
 
-    self._emitter.on("updated", function(ud) {
+    self._emitter.on("updated", function (ud) {
         if (paramd.id && (ud.id !== paramd.id)) {
             return;
         }
@@ -290,28 +287,27 @@ ExpressTransport.prototype.updated = function(paramd, callback) {
  *  you should use <code>iotdb.transport.bind</code>
  *  to effectively replace this function.
  */
-ExpressTransport.prototype.remove = function(paramd) {
-};
+ExpressTransport.prototype.remove = function (paramd) {};
 
 /* -- internals -- */
-var _encode = function(s) {
-    return s.replace(/[\/$%#.\]\[]/g, function(c) {
+var _encode = function (s) {
+    return s.replace(/[\/$%#.\]\[]/g, function (c) {
         return '%' + c.charCodeAt(0).toString(16);
     });
 };
 
-var _decode = function(s) {
+var _decode = function (s) {
     return decodeURIComponent(s);
-}
+};
 
-var _unpack = function(d) {
+var _unpack = function (d) {
     return _.d.transform(d, {
         pre: _.ld_compact,
         key: _decode,
     });
 };
 
-var _pack = function(d) {
+var _pack = function (d) {
     return _.d.transform(d, {
         pre: _.ld_compact,
         key: _encode,
