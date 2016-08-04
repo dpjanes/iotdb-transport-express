aa3e9a44da73171d37827fb48f72b594254aa4b2
David Janes <davidjanes@davidjanes.com>
2016-08-04 15:21:09 -0400

    'use'

--- samples/server.js
+++ samples/server.js
@@ -1,26 +1,20 @@
 const express = require('express');
 const app = express();
 
-/*
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-*/
-
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
+// this is the source 
 const iotdb = require("iotdb");
 iotdb.use("homestar-wemo");
 
-iotdb_transporter = require("../iotdb-transport-iotdb/transporter");
+iotdb_transporter = require("../../iotdb-transport-iotdb/transporter");
 iotdb_transport = iotdb_transporter.make({}, iotdb.connect("WeMoSocket"));
 
-express_transporter = require("./transporter")
+// this is the actual transporter
+express_transporter = require("../transporter")
 express_transport = express_transporter.make({}, app)
 
-express_transport.source.list(iotdb_transport)
-express_transport.source.bands(iotdb_transport)
-express_transport.source.get(iotdb_transport)
-// express_transport.source.pu(iotdb_transport)
+// the actual gets data from the source
+express_transport.use(iotdb_transport)
